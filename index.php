e68bac774cb049a6b9af8656d80d852514f9ef09
naingkokolin <naingkokolin004@gmail.com>
2024-12-19 19:28:15 -0800

    fourth commit

--- index.php
+++ index.php
@@ -1,15 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Food Fusion</title>
-    <link rel="icon" href="./icons/search-icon.svg" type="image/x-icon">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Food Fusion</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="icon" href="./icons/search-icon.svg" type="image/x-icon">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
+
 <body>
   <?php include('navbar.php'); ?>
-  <h1>Delicious Recipes</h1>
+  <h3>Delicious Recipes</h3>
   <img src="./img/chorizo_gnocchi_48255_16x9.jpg" alt="Delicious Recipe" class="home-first-photo">
+  <p>
+    To inspire and empower individuals to embrace home cooking and culinary
+    creativity by providing a vibrant platform for sharing recipes, culinary tips,
+    and fostering a supportive community of food enthusiasts worldwide.
+  </p>
+
+  <h2>Our Blog</h2>
+  <hr>
+
+  <div class="blog-grid">
+    <div>
+      <h6 class="blog-username">Username1</h6>
+      <img src="./img/chorizo_gnocchi_48255_16x9.jpg" alt="Picture holder" class="blog-image">
+    </div>
+
+    <div>
+      <h6 class="blog-username">Username2</h6>
+      <img src="./img/chorizo_gnocchi_48255_16x9.jpg" alt="Picture holder" class="blog-image">
+    </div>
+
+    <div>
+      <h6 class="blog-username">Username3</h6>
+      <img src="./img/chorizo_gnocchi_48255_16x9.jpg" alt="Picture holder" class="blog-image">
+    </div>
+  </div>
+
 </body>
+
 </html>
