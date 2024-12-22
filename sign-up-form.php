e043426105055e1d4ca119b5754a074b03a94f31
naingkokolin004@gmail.com <naingkokolin004@gmail.com>
2024-12-22 08:57:23 +0630

    added login form design and css file

--- sign-up-form.php
+++ sign-up-form.php
@@ -9,17 +9,27 @@
 </head>
 
 <body>
-  <div class="sign-up-form-container">
-    <h3 class="sign-up-header">Sign Up Form</h3>
-    <input class="input-box" type="text" name="firstname" id="" placeholder="First Name">
-    <input class="input-box" type="text" name="lastname" id="" placeholder="Last Name">
-    <input class="input-box" type="email" name="email" id="" placeholder="Email">
-    <input class="input-box" type="password" name="password" id="" placeholder="Password">
 
-    <div class="button-container">
-      <button class="sign-up-button">Sign Up</button>
-       <button class="reset-button">Reset</button>
+  <div class="main-container">
+
+    <div></div>
+
+    <div class="sign-up-form-container">
+      <h3 class="sign-up-header">Sign Up Form</h3>
+      <div class="input-box-container">
+        <input class="input-box" type="text" name="firstname" id="" placeholder="First Name">
+        <input class="input-box" type="text" name="lastname" id="" placeholder="Last Name">
+        <input class="input-box" type="email" name="email" id="" placeholder="Email">
+        <input class="input-box" type="password" name="password" id="" placeholder="Password">
+      </div>
+
+      <div class="button-container">
+        <button class="sign-up-button">Sign Up</button>
+        <button class="reset-button">Reset</button>
+      </div>
     </div>
+
+    <div></div>
 
   </div>
 </body>
